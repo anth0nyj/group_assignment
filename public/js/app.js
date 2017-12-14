d67818eb633e3c72125b9b887ea5f44abb7fd8df
Anthony <ajtalvarez@gmail.com>
2017-12-14 18:58:27 -0500

    Existing comments now populate on page.

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,19 @@ const app = angular.module("PostApp", []);
 
 app.controller("MainController", ["$http", function($http) {
   this.test = "test";
-}]);
+  this.posts = [];
+
+  // Get All Posts
+  $http({
+    method: "get",
+    url: "/posts"
+  }).then(response => {
+    console.log(response.data);
+    this.posts = response.data;
+  }, error => {
+    console.log(error);
+  }).catch(err => {
+    console.log("Catch: ", err);
+  });
+
+}]); //End of MainController
