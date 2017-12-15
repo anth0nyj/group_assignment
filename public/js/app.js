41a2ca893ec8cbc3572558a6de3f473e17d932d0
Mandy Tong <mandytong@Mandys-MacBook-Pro.local>
2017-12-15 00:17:02 -0800

    created edit post route and form working in index.html

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,20 @@ app.controller("MainController", ["$http", function($http) {
   this.posts = [];
   this.post = {};
 
-  // Get All Posts
-  $http({
-    method: "get",
-    url: "/posts"
-  }).then(response => {
-    this.posts = response.data;
-  }, error => {
-    console.error(error);
-  }).catch(err => {
-    console.error("Catch: ", err);
-  });
+  this.loadPosts = () => {
+    $http({
+      method: "get",
+      url: "/posts"
+    }).then(response => {
+      this.posts = response.data;
+    }, error => {
+      console.error(error);
+    }).catch(err => {
+      console.error("Catch: ", err);
+    });
+  }
+
+  this.loadPosts();
 
   // Create New Post
   this.createPost = () => {
@@ -40,6 +43,27 @@ app.controller("MainController", ["$http", function($http) {
     }).then(response => {
       const postIndex = this.posts.findIndex(post => post._id === postToDelete._id);
       this.posts.splice(postIndex, 1);
+    }, error => {
+      console.error(error);
+    }).catch(err => {
+      console.error("Catch: ", err);
+    })
+  }
+
+  this.showEdit = ( post) => {
+    this.editData={};
+    this.showForm=post._id;
+  }
+
+  // Edit Post
+  this.editPost = ( post ) => {
+    $http({
+      method: "PUT",
+      url: "/posts/" + post._id,
+      data: this.editData
+    }).then(data => {
+      this.loadPosts();
+    console.log(data);
     }, error => {
       console.error(error);
     }).catch(err => {
