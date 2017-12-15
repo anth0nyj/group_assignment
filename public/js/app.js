105ed3516e7b0baa61076fbc19732ef530f27448
Anthony <ajtalvarez@gmail.com>
2017-12-15 01:00:42 -0500

    Implemented fully functional edit feature.

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,23 @@ const app = angular.module("PostApp", []);
 app.controller("MainController", ["$http", function($http) {
   this.posts = [];
   this.post = {};
+  this.showForm = false;
 
   // Get All Posts
-  $http({
-    method: "get",
-    url: "/posts"
-  }).then(response => {
-    this.posts = response.data;
-  }, error => {
-    console.error(error);
-  }).catch(err => {
-    console.error("Catch: ", err);
-  });
+  this.loadPosts = () => {
+    $http({
+      method: "get",
+      url: "/posts"
+    }).then(response => {
+      this.posts = response.data;
+    }, error => {
+      console.error(error);
+    }).catch(err => {
+      console.error("Catch: ", err);
+    });
+  }
+
+  this.loadPosts();
 
   // Create New Post
   this.createPost = () => {
@@ -40,6 +45,31 @@ app.controller("MainController", ["$http", function($http) {
     }).then(response => {
       const postIndex = this.posts.findIndex(post => post._id === postToDelete._id);
       this.posts.splice(postIndex, 1);
+    }, error => {
+      console.error(error);
+    }).catch(err => {
+      console.error("Catch: ", err);
+    })
+  }
+
+  // Show Edit Form
+  this.showEdit = (post) => {
+    this.editData = {};
+    this.showForm = post._id;
+  }
+
+  // Edit Post
+  this.editPost = (post) => {
+    console.log("Editing: ", post._id);
+    $http({
+      method: "put",
+      url: "/posts/" + post._id,
+      data: this.editData
+    }).then(response => {
+      console.log(this.editData);
+      this.loadPosts();
+      this.showForm = {};
+      console.log("Response: ", response);
     }, error => {
       console.error(error);
     }).catch(err => {
